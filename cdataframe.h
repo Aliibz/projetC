71ac58b00ffed672cfa79f0c8a5d047d28f86fa3
BuIIDOSER <ahacques04@gmail.com>
2024-04-21 19:43:15 +0200

    patch

--- cdataframe.h
+++ cdataframe.h
@@ -10,3 +10,11 @@ typedef struct {
 CDataframe* create_empty_dataframe(int nombre_colonnes);
 
 void fill_dataframe_from_user_input(CDataframe* dataframe);
+
+void fill_dataframe_with_predefined_values(CDataframe* dataframe);
+
+void print_dataframe(CDataframe* dataframe);
+
+void print_partial_rows(CDataframe* dataframe, int limite);
+
+void print_partial_columns(CDataframe* dataframe, int limite);
